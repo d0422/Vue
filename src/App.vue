5a4fb886053711c3008008463f517e10fa70d2bf
d0422 <rlfehd2013@naver.com>
2022-07-23 00:53:21 +0900

    :zap: 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 :style="서타일">XX원룸</h4>
+    <p>{{price1}}만원</p>
+  </div>
+  <div>
+    <h4 >XX원룸</h4>
+    <p >{{price2}}만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return{
+      price1:60,
+      price2:90,
+      서타일 :"color : tomato",
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
