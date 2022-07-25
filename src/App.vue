cd3b7c4792d69a2999a6880d945b1815d912c7f7
d0422 <rlfehd2013@naver.com>
2022-07-25 22:31:44 +0900

    :sparkles: 모달창 만들어봄

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸냐">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p @click="모달창열렸냐=false">내용임</p>
+    </div>
+  </div> 
+
   <div class="menu">
     <a v-for="메뉴목록 in 메뉴들" :key="메뉴목록">{{메뉴목록}}</a>
   </div>
   <div>
-    <h4 v-for ="동네 in 동네들" :key="동네">{{동네}}원룸</h4>
-    <p>50만원</p>
-    <button @click="신고수올리기">허위매물신고</button>
-    <span>신고수 : {{신고수}} </span>
+    <div v-for="i in 3" :key="i" @click="모달창열렸냐=true">
+      <h4 >{{동네들[i]}} 원룸</h4>
+      <img src="./images/room0.jpg"/>
+      <button @click="신고수[i]++">신고</button>
+      <div>신고수 : {{신고수[i]}} </div>
+    </div>
   </div>
   <button v-on:click="신고수초기화하기">Reset</button>
 </template>
@@ -16,19 +25,20 @@ export default {
   name: 'App',
   data(){
     return{
-      신고수: 0,
+      모달창열렸냐 : false,
+      신고수: [0,0,0,0],
       메뉴들 : ["Home","Shop","About"],
-      동네들:["역삼동","천호동","마포구"],
+      동네들:["","역삼동","천호동","마포구"],
+      이미지:["", './images/room0.jpg', './images/room1.jpg', './images/room2.jpg'],
     }
   },
   methods :{
-    신고수올리기(){
-      this.신고수=this.신고수+1;
-    },
     신고수초기화하기(){
-      this.신고수=0;
+      for(var i=0; i<4; i++){
+        this.
+        신고수[i]=0;
     }
-    
+    }
   },
   components: {
   }
@@ -36,6 +46,12 @@ export default {
 </script>
 
 <style>
+body{
+  margin:0;
+}
+div{
+  box-sizing:border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -51,5 +67,18 @@ export default {
 .menu a{
   color : white;
   padding: 10px;
+}
+.black-bg{
+  width:100%;
+  height:100%;
+  background: rgba(0,0,0,0.5);
+  position:fixed;
+  padding:20px;
+}
+.white-bg{
+  width:100%;
+  background:white;
+  border-radius:8px;
+  padding:20px;
 }
 </style>
